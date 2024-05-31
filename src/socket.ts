47d1be2e717ff92ca79d64858989ec0d7ba523c4
Shubham Tambwe <72244135+shubhu0767@users.noreply.github.com>
2024-05-31 13:58:05 +0530

    add production URL in Socket.io

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const URL = process.env.NODE_ENV === "production" ? 'production link' : 'http://localhost:5000'
+const URL = process.env.NODE_ENV === "production" ? 'https://online-drawing-tool-backend.onrender.com' : 'http://localhost:5000'
 
 export const socket = io(URL);
